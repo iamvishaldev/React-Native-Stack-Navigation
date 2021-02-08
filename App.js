717e98d6c1dee01ff1d4fdf1c18622bd8cc5115a
Vishal Yadav <yvishal706@gmail.com>
2021-02-08 13:53:48 +0530

    First Commit

--- App.js
+++ App.js
@@ -1,14 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
+import 'react-native-gesture-handler';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {NavigationContainer} from '@react-navigation/native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen 
+        name="Home"
+        component={HomeScreen}
+        />
+        <Stack.Screen
+        name="Details"
+        component={DetailsScreen}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
+}
+
+const HomeScreen = ({navigation})=>{
+
+  let data = {name: "vishal",email:"yvishal706@gmail.com",address:"Delhi"}
+
+  return(
+    <View style={{flex:1,justifyContent:"center",alignItems:"center"}}>
+      <Text>Welcome To Home Screen</Text>
+      <Button title="Go to the Details"  onPress={()=>navigation.push('Details',data)}/>
+    </View>
+  )
+}
+
+const DetailsScreen=({route})=>{ //route as props to get the value from Home
+  console.warn(route.params);
+  let data = route.params
+  return(
+    <View style={{flex:1,justifyContent:"center",alignItems:"center"}}>
+      <Text>Welcome To Detail Screen</Text>
+      <Text style={{fontSize:30}}>{data.name}</Text>
+      <Text style={{fontSize:30}}>{data.email}</Text>
+      <Text style={{fontSize:30}}>{data.address}</Text>
+
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
